ccb6a0c40dadafcb58ac5514c9492c6832c39181
Daniel Thompson <thompsdj@thompsdj-5520.rose-hulman.edu>
2022-01-28 14:22:59 -0500

    Got files reading in

--- Homework One/src/part1/PartOneRunner.java
+++ Homework One/src/part1/PartOneRunner.java
@@ -3,6 +3,19 @@ package part1;
 public class PartOneRunner {
 
 	public static void main(String[] args) {
+		UserInputs inputs = new UserInputs();
+		
+		inputs.askForInputs();
+		
+		System.out.println(inputs.getPuzzleSize());
+		System.out.println(inputs.getInitialBoard());
+		System.out.println(inputs.getGoalBoard());
+		System.out.println(inputs.getEvaluationOption());
+		System.out.println(inputs.getHeuristicOption());
+		
+		AStarSliding algorithm = new AStarSliding();
+		
+		
 		
 	}
 
